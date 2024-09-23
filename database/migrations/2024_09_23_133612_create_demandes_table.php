926446cb12220c7315af4dd2ed68980a3cd827e0
Kaboufl <r.laurence@geeklab-factory.com>
2024-09-23 16:00:46 +0200

    first-commit

---
+++ database/migrations/2024_09_23_133612_create_demandes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('demandes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('annonce_id');
+            $table->unsignedBigInteger('user_id');
+            $table->text('message');
+            $table->enum('statut', ['pending', 'denied', 'accepted']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('demandes');
+    }
+};
