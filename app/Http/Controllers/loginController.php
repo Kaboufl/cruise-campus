65370fce49de3a706a9c2df3be669efbf3ac886d
Kaboufl <r.laurence@geeklab-factory.com>
2024-09-27 12:25:34 +0200

    test deploiement nodeJS from official source

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -47,7 +47,7 @@ class loginController extends Controller
             'ville' => 'required|string',
             'telephone' => 'required|numeric',
             'campus' => 'required|numeric',
-            'password' => 'required|min:8|regex:/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/',
+            'password' => 'required|min:8',
             'password_confirmation' => 'required|same:password'
         ]);
 
